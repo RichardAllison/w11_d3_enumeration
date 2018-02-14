aa45763fbf585eb6b41a0d04c0885bad94ac1e2e
Richard Allison <richard.allison.18@outlook.com>
2018-02-14 20:03:49 +0000

    Added function to insert item at particular index in array, passes test

--- array_tasks.js
+++ array_tasks.js
@@ -11,9 +11,10 @@ const arrayTasks = {
     return newArray;
 	},
 
-	// insertAt: function (arr, itemToAdd, index) {
-
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+    arr.splice(index, 0, itemToAdd);
+    return arr;
+	},
 
 	// square: function (arr) {
 
